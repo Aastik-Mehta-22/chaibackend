d012c0e68dccc2e87164350faeecfd14d714056d
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-06-25 04:38:04 +0530

    comments add in ApiError.js

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,18 +1,18 @@
-class ApiError extends Error {
+class ApiError extends Error { //It extends the built-in Error class, which allows it to inherit error handling behavior.
     constructor(
         statusCode,
-        message= "Something went wrong",
+        message= "Something went wrong", 
         errors = [],
         stack = ""
     ){
-        super(message)
+        super(message) // for overwrite success message
         this.statusCode = statusCode
         this.data = null
-        this.message = message
-        this.success = false;
+        this.message = message 
+        this.success = false; 
         this.errors = errors
 
-        if (stack) {
+        if (stack) { // The purpose of this logic is to handle the stack trace associated with an error
             this.stack = stack
         } else{
             Error.captureStackTrace(this, this.constructor)
