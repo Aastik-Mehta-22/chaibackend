4458c0b5affb5d7dc75db3c390d4df84409ba1bf
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-06-25 04:28:02 +0530

    "Fixed newline at end of file in app.js and added comment in ApiResponse.js"

--- src/app.js
+++ src/app.js
@@ -16,4 +16,4 @@ app.use(express.urlencoded({extended: true , limit:"16kb"}))
 app.use(express.static("public")) // for stroing static files 
 app.use(cookieParser())
 
-export { app }
+export {app}
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,7 +3,7 @@ class ApiResponse {
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statusCode < 400
+        this.success = statusCode < 400 // basically status less than 400 shows a success
     }
 }
 
