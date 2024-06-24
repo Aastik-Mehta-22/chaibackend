a8408f63ec72f289dd698256143b3e8fa4871e39
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-06-24 22:27:04 +0530

    Utils Code added standard set

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
-
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './.env'
 })
 
+
+
 connectDB()
+.then(() => {
+    // app.on("error", (error) => {
+    //     console.error("Uncaught application error:", error); 
+    // });
+
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection fail !! ",err);
+})
+
+
+
+
 
 
 
